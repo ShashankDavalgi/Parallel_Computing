9bbe1e14fec47f0f27ec5fe5b12a06d22ab2fbce
ShashankDavalgi <shshnkdavalgi10@gmail.com>
2017-04-20 13:25:01 +0530

    Update openmp.c

--- PrimeCounter/openmp.c
+++ PrimeCounter/openmp.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+#include<omp.h>
 
+void main()
+{
+	int i=0;
+	int s=2;
+	int e=50000;
+
+	int j=0,count1=0,pc=0;
+
+  //the open mp model which parallelises the for loop and applies reduction
+  
+	#pragma omp parallel for reduction(+:pc)
+	for(i=2; i<=e; i++) {
+			
+			int count1 = 0;
+			
+			for(j=1; j<=i; j++) {
+								
+				if((i%j) == 0) {
+					
+					count1++;
+				}
+			}
+		if(count1 == 2) {
+				
+				pc++;
+				//System.out.print("  " + i);
+			}
+}
+printf("%d\n",pc);
+}
