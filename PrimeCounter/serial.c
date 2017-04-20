5ffe1a24ccfa06911ffc653d8ea69004e1172a2f
ShashankDavalgi <shshnkdavalgi10@gmail.com>
2017-04-20 11:52:58 +0530

    Update serial.c

--- PrimeCounter/serial.c
+++ PrimeCounter/serial.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
 
+void main()
+{
+	int i=0;
+	int s=2;
+	int e=50000;
+
+	int j=0,count1=0,pc=0;
+
+  //Outer for loop goes from 2 to the number chosen. the loop starts from 2 because 0 and 1 are not prime
+	
+	for(i=2; i<=e; i++) {
+			
+			int count1 = 0;
+		
+    // inner for loop finds whether it is prime or not for every number.
+    
+			for(j=1; j<=i; j++) {
+								
+        //If the remainder is 0, then there is a common factor.
+        
+				if((i%j) == 0) {
+					
+					count1++;
+				}
+			}
+		if(count1 == 2) {
+				
+      // incrememnting the prime counter
+      
+				pc++;
+				//System.out.print("  " + i);
+			}
+}
+printf("%d\n",pc);
+}
